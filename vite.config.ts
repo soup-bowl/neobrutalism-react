b128b3ff69e4e1baf11c569f15214e5ddbbb1e97
soup-bowl <code@soupbowl.io>
2025-05-07 21:57:24 +0000

    Build with TS defs.

--- vite.config.ts
+++ vite.config.ts
@@ -1,13 +1,16 @@
 import { defineConfig } from "vite"
 import react from "@vitejs/plugin-react"
+import dts from "vite-plugin-dts"
+import { resolve } from "path"
 
 export default defineConfig({
-	plugins: [react()],
+	plugins: [react(), dts({ insertTypesEntry: true })],
 	build: {
 		lib: {
-			entry: "src/index.ts",
+			entry: resolve(__dirname, "src/index.ts"),
 			name: "UILibrary",
 			fileName: (format) => `ui-library.${format}.js`,
+			formats: ["es", "umd"],
 		},
 		rollupOptions: {
 			// Ensure external dependencies are not bundled

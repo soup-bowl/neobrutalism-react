5377e28e783f35d9880a891d66fcecea9345c54e
soup-bowl <code@soupbowl.io>
2025-05-07 19:24:12 +0000

    Button without reakit.

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,12 +1,35 @@
 import React from "react"
 
-interface ButtonProps {
+export type BrutalistButtonProps = {
+	variant?: "primary" | "secondary"
+	size?: "regular" | "small"
 	label: string
-	onClick: () => void
-}
+	children: React.ReactNode
+	style?: React.CSSProperties
+} & React.ComponentProps<"button">
 
-const Button: React.FC<ButtonProps> = ({ label, onClick }) => {
-	return <button onClick={onClick}>{label}</button>
+const Button: React.FC<BrutalistButtonProps> = ({ variant = "primary", size = "regular", label, style, ...props }) => {
+	const boxy = (size: string) =>
+		`${size}px ${size}px 0px rgb(0, 0, 0), ${size}px ${size}px 0px rgb(0, 0, 0), ${size}px ${size}px 0px rgb(0, 0, 0)`
+	const defaultStyles: React.CSSProperties = {
+		cursor: "pointer",
+		backgroundColor: "#ffbd94",
+		paddingRight: size === "regular" ? "2rem" : ".5rem",
+		paddingLeft: size === "regular" ? "2rem" : ".5rem",
+		paddingTop: size === "regular" ? ".5rem" : "0",
+		paddingBottom: size === "regular" ? ".5rem" : "0",
+		height: size === "regular" ? "2.75rem" : "1.5rem",
+		borderWidth: 2,
+		borderColor: "black",
+		borderStyle: "solid",
+		boxShadow: size === "regular" ? boxy("4") : boxy("2"),
+	}
+
+	return (
+		<button style={{ ...defaultStyles, ...style }} {...props}>
+			{label}
+		</button>
+	)
 }
 
 export default Button
--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -1,28 +1,38 @@
-import React from "react"
-import Button from "../components/Button"
+import Button, { BrutalistButtonProps } from "../components/Button"
 
 export default {
 	title: "Example/Button",
 	component: Button,
+	argTypes: {
+		// Add controls for props if needed
+		onClick: { action: "clicked" },
+		disabled: { control: "boolean" },
+	},
 }
 
-const Template = (args) => <Button {...args} />
+const Template = (args: BrutalistButtonProps & { label: string }) => <Button {...args} />
 
-export const Primary = Template.bind({})
-Primary.args = {
-	label: "Primary Button",
-	onClick: () => alert("Primary Button Clicked!"),
+export const Primary = {
+	...Template.bind({}),
+	args: {
+		label: "Primary Button",
+		onClick: () => alert("Primary Button Clicked!"),
+	},
 }
 
-export const Secondary = Template.bind({})
-Secondary.args = {
-	label: "Secondary Button",
-	onClick: () => alert("Secondary Button Clicked!"),
+export const Secondary = {
+	...Template.bind({}),
+	args: {
+		label: "Secondary Button",
+		onClick: () => alert("Secondary Button Clicked!"),
+	},
 }
 
-export const Disabled = Template.bind({})
-Disabled.args = {
-	label: "Disabled Button",
-	onClick: () => alert("Disabled Button Clicked!"),
-	disabled: true,
+export const Disabled = {
+	...Template.bind({}),
+	args: {
+		label: "Disabled Button",
+		onClick: () => alert("Disabled Button Clicked!"),
+		disabled: true,
+	},
 }

cd6ab6b52208257a23535f8bae22ed2c364ba3ef
soup-bowl <code@soupbowl.io>
2025-05-12 22:08:04 +0000

    lkjflkgds

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,16 @@
-export { default as Button } from "./components/Button"
+export {
+	default as Button,
+	AttentionButton,
+	AttentionLink,
+	ButtonGroup,
+	ScrollButtons,
+	ScrollToTopButton,
+} from "./components/Button"
 export { default as Card } from "./components/Card"
 export { default as Banner } from "./components/Banner"
+export { Modal } from "./components/Modal"
+
+export type { NormalButtonProps } from "./components/Button"
+export type { BrutalistCardProps } from "./components/Card"
+export type { BrutalistBannerProps } from "./components/Banner"
+export type { ModalProps } from "./components/Modal"
